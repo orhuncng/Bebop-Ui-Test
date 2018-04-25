bca0c9213e27c568f3f7e96df613e0b26597f95b
orhuncng <altunluorhun@gmail.com>
2018-04-25 19:22:11 +0300

    Merge branch 'master' of https://github.com/orhuncng/Bebop-Ui-Test
    # Conflicts:
    #	app/app.iml

--- app/src/main/java/com/trio/dronetest/DataFilterer.java
+++ app/src/main/java/com/trio/dronetest/DataFilterer.java
@@ -4,19 +4,15 @@ package com.trio.dronetest;
  * Created by orhun on 4/17/2018.
  */
 
-public class DataFilterer
-{
-
+public class DataFilterer {
     static final float ALPHA = 0.8f;
 
     public float[] lowPassFilter(float[] output, float[] input)
     {
-
         output[0] = ALPHA * output[0] + (1 - ALPHA) * input[0];
         output[1] = ALPHA * output[1] + (1 - ALPHA) * input[1];
         output[2] = ALPHA * output[2] + (1 - ALPHA) * input[2];
 
         return output;
-
     }
 }

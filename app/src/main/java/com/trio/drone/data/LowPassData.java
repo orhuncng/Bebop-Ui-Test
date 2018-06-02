6ac1138954597f4b80b6894533363317a2ef1880
obilgen35 <oguzbilgen@gmail.com>
2018-06-02 13:13:18 +0300

    Low pass filter updates

--- app/src/main/java/com/trio/drone/data/LowPassData.java
+++ app/src/main/java/com/trio/drone/data/LowPassData.java
@@ -1,5 +1,7 @@
 package com.trio.drone.data;
 
+import android.util.Log;
+
 /**
  * Created by Hp on 1.06.2018.
  */
@@ -14,10 +16,15 @@ public class LowPassData implements FilterData {
 
     @Override
     public float[] get(float[] newValues) {
+
+        Log.e("raw", Float.toString(newValues[2]));
+
         history[0] = smoothingPerc * history[0] + (1f - smoothingPerc) * newValues[0];
         history[1] = smoothingPerc * history[1] + (1f - smoothingPerc) * newValues[1];
         history[2] = smoothingPerc * history[2] + (1f - smoothingPerc) * newValues[2];
 
+
+        Log.e("filtered", Float.toString(history[2]));
         return history;
     }
 }
